599ccb8fa72f29f418f6ac5d6c0e70dc31c1cae9
Tanmay Haldankar <tanmay.n.haldankar99@gmail.com>
2022-12-13 21:11:47 -0500

    Task 2.0.9 Creating subscriber to subscribe to odom of one turtlebot

--- src/pubsub.cpp
+++ src/pubsub.cpp
@@ -8,7 +8,7 @@ using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 using TWIST = geometry_msgs::msg::Twist;
-
+using ODOM = nav_msgs::msg::Odometry;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
@@ -21,12 +21,20 @@ class RoomBa : public rclcpp::Node {
     auto pubTopicName = "/tb0/cmd_vel";
     publisher_ = this->create_publisher<TWIST>(pubTopicName, 10);
 
+    auto subTopicName = "/tb0/odom";
+    auto subCallback = std::bind(&RoomBa::subscribe_callback, this, _1);
+    subscription_ =
+        this->create_subscription<ODOM>(subTopicName, 10, subCallback);
+
     auto processCallback = std::bind(&RoomBa::process_callback, this);
     timer_ = this->create_wall_timer(100ms, processCallback);
   }
 
 
  private:
+    void subscribe_callback(const ODOM &msg) {  
+      std::cout << "The pose in x is " << msg.pose.pose.position.x<<std::endl;
+    }
 
     void process_callback() {
         auto message = TWIST();
@@ -35,6 +43,7 @@ class RoomBa : public rclcpp::Node {
         publisher_->publish(message);
     }
 
+  rclcpp::Subscription<ODOM>::SharedPtr subscription_;
   rclcpp::Publisher<TWIST>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
 
