90c4f3a44c204b9e66a97673c2db8210f7c5bf78
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-12-15 00:38:44 -0500

    Task 2.2.2 Creating ROS tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,12 @@
 
 #include <gtest/gtest.h>
+#include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
   std::cout << "DONE SHUTTING DOWN" << std::endl;
+  rclcpp::shutdown();
   return result;
 }
---
+++ test/pubsub_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include <rclcpp/rclcpp.hpp>
+
+#include "std_msgs/msg/string.hpp"
+#include "../include/pubsub.hpp"
+
+
+class TestPublisher : public testing::Test {
+ protected:
+  rclcpp::Node::SharedPtr node_;
+};
+
+TEST_F(TestPublisher, test_num_publishers) {
+  node_ = rclcpp::Node::make_shared("test_pub");
+  auto test_pub = node_->create_publisher<std_msgs::msg::String>
+                    ("I_am_Publishing", 10.0);
+
+  auto pub = node_->count_publishers("I_am_Publishing");
+  EXPECT_EQ(0, static_cast<int>(pub));
+}
