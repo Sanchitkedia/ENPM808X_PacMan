215d037e01a8d15f447715a34bbf7612d51bb541
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-12-15 00:51:13 -0500

    Task 2.2.5 and 2.2.6 Adding sqare and triangle functions

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -7,6 +7,8 @@ Trajectory::Trajectory() {
   xCenter = 0;
   yCenter = 0;
   radius = 9.6;
+  circumRadius = 9.6;
+  sideLength = 9.6;
 }
 
 void Trajectory::setCenter(double x, double y) {
@@ -28,4 +30,47 @@ vector<vector<double>> Trajectory::circleTrajectory() {
     angle += angleIncrement;
   }
   return circleTrajectory;
+}
+
+vector<vector<double>> Trajectory::squareTrajectory() {
+  vector<vector<double>> squareTrajectory(numberOfRobots, vector<double>(2, 0));
+  double x = xCenter - sideLength / 2;
+  double y = yCenter - sideLength / 2;
+  for (int i = 0; i < numberOfRobots; i++) {
+    if (i < numberOfRobots / 4) {
+      x = x + sideLength / (numberOfRobots / 4);
+    } else if (i < numberOfRobots / 2) {
+      y = y + sideLength / (numberOfRobots / 4);
+    } else if (i < 3 * numberOfRobots / 4) {
+      x = x - sideLength / (numberOfRobots / 4);
+    } else {
+      y = y - sideLength / (numberOfRobots / 4);
+    }
+    squareTrajectory[i][0] = x;
+    squareTrajectory[i][1] = y;
+  }
+  return squareTrajectory;
+}
+
+vector<vector<double>> Trajectory::triangleTrajectory() {
+  vector<vector<double>> triangleTrajectory(numberOfRobots,
+                                            vector<double>(2, 0));
+  double x = xCenter - circumRadius;
+  double y = yCenter + (3 * circumRadius / 2 - circumRadius);
+  for (int i = 0; i < numberOfRobots; i++) {
+    if (i < numberOfRobots / 3) {
+      x = x + circumRadius / (numberOfRobots / 3);
+      y = y - circumRadius / (numberOfRobots / 3);
+    } else if (i < 2 * numberOfRobots / 3) {
+      x = x + circumRadius / (numberOfRobots / 3);
+      y = y + circumRadius / (numberOfRobots / 3);
+    } else {
+      x = x - 2 * (circumRadius / (numberOfRobots / 3));
+      y = y;
+    }
+    triangleTrajectory[i][0] = x;
+    triangleTrajectory[i][1] = y;
+  }
+
+  return triangleTrajectory;
 }
