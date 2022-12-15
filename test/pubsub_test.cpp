0df30fcd2c9bb90f91dea8aec81c73ca9812e853
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-12-15 10:05:50 -0500

    Task 2.3.4 Creating new and updating old test cases

--- test/pubsub_test.cpp
+++ test/pubsub_test.cpp
@@ -6,16 +6,27 @@
 #include "../include/pubsub.hpp"
 
 
-class TestPublisher : public testing::Test {
+class TestPubSub : public testing::Test {
  protected:
   rclcpp::Node::SharedPtr node_;
 };
 
-TEST_F(TestPublisher, test_num_publishers) {
+TEST_F(TestPubSub, test_num_publishers) {
   node_ = rclcpp::Node::make_shared("test_pub");
   auto test_pub = node_->create_publisher<std_msgs::msg::String>
                     ("I_am_Publishing", 10.0);
-
+    
   auto pub = node_->count_publishers("I_am_Publishing");
-  EXPECT_EQ(0, static_cast<int>(pub));
+  EXPECT_EQ(1, static_cast<int>(pub));
+}
+
+TEST_F(TestPubSub, test_num_subscribers) {
+  node_ = rclcpp::Node::make_shared("test_sub");
+  auto test_sub = node_->create_subscription<std_msgs::msg::String>("I_am_Publishing", 10.0,
+    [](const std_msgs::msg::String::SharedPtr msg) {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "I heard: '%s'", msg->data.c_str());
+    });
+
+  auto sub = node_->count_subscribers("I_am_Subscribing");
+  EXPECT_EQ(0, static_cast<int>(sub));
 }
--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,28 @@
 #include <utility>
 #include <vector>
 
-TEST(CircleTest, test1) {
-  std::vector<std::vector<double>> trajectory = {{0, 0}, {0, 0}, {0, 0}};
-    EXPECT_EQ(trajectory[0][0], 0);  // expected location x coordinate
-    EXPECT_EQ(trajectory[0][1], 0);  // expected location y coordinate
+# include "../include/trajectory.hpp"
+
+TEST(AlgorithmTest,CircleTest) {
+  Trajectory traj;
+  traj.setCenter(0,0);
+  traj.setNumberOfRobots(24);
+  std::vector<std::vector<double>> circle = traj.circleTrajectory();
+  EXPECT_EQ(circle.size(),24);
+}
+
+TEST(AlgorithmTest,SquareTest) {
+  Trajectory traj;
+  traj.setCenter(0,0);
+  traj.setNumberOfRobots(24);
+  std::vector<std::vector<double>> square = traj.squareTrajectory();
+  EXPECT_EQ(square.size(),24);
+}
+
+TEST(AlgorithmTest,TriangleTest) {
+  Trajectory traj;
+  traj.setCenter(0,0);
+  traj.setNumberOfRobots(24);
+  std::vector<std::vector<double>> triangle = traj.triangleTrajectory();
+  EXPECT_EQ(triangle.size(),24);
 }
